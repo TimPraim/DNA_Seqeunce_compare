8f4d0d87f4bc526b946bf80d885c24072ee694ed
TimPraim <tpraim@bluewin.ch>
2024-09-18 16:03:42 +0200

    Nukliotide zählen in beiden Sequenzen der Mutation

--- FASTA_Read.py
+++ FASTA_Read.py
@@ -1,11 +1,40 @@
 import sys
 from Bio import SeqIO
 
-fasta_file = "gene.fna"
+fasta_file = "gene.fna" #Pfad zur FASTA Datei
 
-# Die FASTA-Datei öffnen und die Sequenz lesen
+
+def count_nucleotides(dna_sequence_1):
+    g_seq = 0
+    t_seq = 0
+    a_seq = 0
+    c_seq = 0
+
+    # Durchlaufen der DNA-Sequenz und Zählen der Nukleotide (addieren)
+    for nucleotide in dna_sequence_1:
+        if nucleotide == 'G':
+            g_seq += 1
+        elif nucleotide == 'T':
+            t_seq += 1
+        elif nucleotide == 'A':
+            a_seq += 1
+        elif nucleotide == 'C':
+            c_seq += 1
+
+    return g_seq, t_seq, a_seq, c_seq
+
+
+
+# Die FASTA-Datei öffnen und die Sequenzen lesen
 with open(fasta_file, "r") as file:
     for record in SeqIO.parse(file, "fasta"):
-        # Die ersten 10 Nukleotide aus der Sequenz ausgeben
-        print(record.seq[:10])
-        break  # Nur den ersten Eintrag verarbeiten
+        # Zähle die Nukleotide in der aktuellen Sequenz
+        g_seq, t_seq, a_seq, c_seq = count_nucleotides(str(record.seq))
+
+        # Ergebnisse für die aktuelle Sequenz ausgeben
+        print(f"Sequenz-ID: {record.id}")
+        print(f"Anzahl von A: {a_seq}")
+        print(f"Anzahl von T: {t_seq}")
+        print(f"Anzahl von C: {c_seq}")
+        print(f"Anzahl von G: {g_seq}")
+        print("----------")
